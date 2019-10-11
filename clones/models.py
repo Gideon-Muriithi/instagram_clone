c83403ecf1e314e17293c04228e713cd701429bb
Gideon-Muriithi <gideongakenge@gmail.com>
2019-10-11 14:02:45 +0300

    Create image and profile models.

--- clones/models.py
+++ clones/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Image(models.Model):
+    image = models.ImageField(upload_to='images/')
+    image_name = models.CharField(max_length=20)
+    image_caption = models.CharField(max_length=20)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    likes = models.CharField()
+    comments = models.TextField(max_length=70)
+
+class Profile(models.Model):
+    profile_photo = models.ImageField(default='default.jpg', upload_to='profile_pics/')
+    bio = models.TextField()
