fc16cf212765b257fc67b428e7cd392010af6feb
Gideon-Muriithi <gideongakenge@gmail.com>
2019-10-11 15:58:26 +0300

    Add more properties to the models and also modify others.

--- clones/models.py
+++ clones/models.py
@@ -1,13 +1,25 @@
 from django.db import models
 
-class Image(models.Model):
-    image = models.ImageField(upload_to='images/')
-    image_name = models.CharField(max_length=20)
-    image_caption = models.CharField(max_length=20)
-    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    likes = models.CharField()
-    comments = models.TextField(max_length=70)
 
 class Profile(models.Model):
     profile_photo = models.ImageField(default='default.jpg', upload_to='profile_pics/')
     bio = models.TextField()
+
+
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='images/')
+    image_name = models.CharField(max_length=20)
+    image_caption = models.CharField(max_length=100)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    likes = models.ManyToManyField('Profile', default=False, blank=True, related_name='likes')
+    comments = models.TextField(max_length=70)
+
+    def __str__(self):
+        return self.image_caption
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
