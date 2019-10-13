2855cdf4a2ea1a8a776d6765f44605f5e005f652
Gideon-Muriithi <gideongakenge@gmail.com>
2019-10-13 14:13:03 +0300

    Add comment model and  add more fields to image and profile models.

--- clones/models.py
+++ clones/models.py
@@ -1,9 +1,25 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from PIL import Image
+from django.utils import timezone
+from tinymce.models import HTMLField
 
 class Profile(models.Model):
     profile_photo = models.ImageField(default='default.jpg', upload_to='profile_pics/')
     bio = models.TextField()
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    def save(self):
+        super().save()
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
 
 
 
@@ -12,8 +28,9 @@ class Image(models.Model):
     image_name = models.CharField(max_length=20)
     image_caption = models.CharField(max_length=100)
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    likes = models.ManyToManyField('Profile', default=False, blank=True, related_name='likes')
-    comments = models.TextField(max_length=70)
+    likes = models.BooleanField(default=False)
+    date_posted = models.DateTimeField(default=timezone.now)
+    profile = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.image_caption
@@ -23,3 +40,14 @@ class Image(models.Model):
 
     def delete_image(self):
         self.delete()
+
+
+class Comments(models.Model):
+    comment = HTMLField()
+    posted_on = models.DateTimeField(auto_now=True)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def save_comment(self):
+        self.save()
+    
