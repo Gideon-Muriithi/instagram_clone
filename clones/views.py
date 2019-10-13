789f08b16eab41ec434ca39b0ed101f7ed695419
Gideon-Muriithi <gideongakenge@gmail.com>
2019-10-13 22:40:33 +0300

    Add view functions.

--- clones/views.py
+++ clones/views.py
@@ -1,4 +1,75 @@
 from django.shortcuts import render
+from .models import Image, Profile,Comment
+from django.contrib.auth.models import User
+from .forms import CommentForm, ImageForm
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     return render(request, 'base.html')
+
+@login_required(login_url='/accounts/login/')
+def all_images(request):
+    all_images = Image.objects.all()
+    all_users = Profile.objects.all()
+    next = request.GET.get('next')
+    if next: return redirect(next)
+    return render(request, 'images.html',  {"all_images": all_images}, {"all_users":all_users})
+
+
+def profile(request, username):
+    profile = User.objects.get(username=username)
+    try:
+        profile_details = Profile.get_by_id(profile.id)
+    except:
+        profile_details = Profile.filter_by_id(profile.id)
+    images = Image.get_profile_images(profile.id)
+    title = f'@{profile.username} Instagram photos and videos'
+
+    return render(request, 'profiles/profile.html', {'title':title, 'profile':profile, 'profile_details':profile_details, 'images':images})
+
+@login_required(login_url='/accounts/login')
+def single_image(request, image_id):
+    image = Image.get_image_id(image_id)
+    comments = Comment.get_comments_by_images(image_id)
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.image = image
+            comment.user = request.user
+            comment.save()
+            return redirect('single_image', image_id=image_id)
+    else:
+        form = CommentForm()
+        
+    return render(request, 'images.html', {'image':image, 'form':form, 'comments':comments})
+
+@login_required(login_url='/accounts/login')
+def edit_profile(request):
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            edit = form.save(commit=False)
+            edit.user = request.user
+            edit.save()
+            return redirect('edit_profile')
+    else:
+        form = ProfileForm()
+
+    return render(request, 'profiles/edit_profile.html', {'form':form})
+
+
+@login_required(login_url='/accounts/login')
+def upload_image(request):
+    if request.method == 'POST':
+        form = ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            upload = form.save(commit=False)
+            upload.profile = request.user
+            upload.save()
+            return redirect('profile', username=request.user)
+    else:
+        form = ImageForm()
+    
+    return render(request, 'profiles/image_upload.html', {'form':form})
