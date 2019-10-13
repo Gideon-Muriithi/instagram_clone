28ab2f60f37c4f6487183003eba59b42130a6520
Gideon-Muriithi <gideongakenge@gmail.com>
2019-10-13 14:13:47 +0300

    Add google font CDN.

--- instagram/settings.py
+++ instagram/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'crispy_forms',
     'bootstrap4',
+    'tinymce',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
