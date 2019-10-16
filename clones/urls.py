e8d47106f92f41b0f2913f4ad53e5e0ee4a28258
Gideon-Muriithi <gideongakenge@gmail.com>
2019-10-16 09:11:31 +0300

    Add search url.

--- clones/urls.py
+++ clones/urls.py
@@ -11,6 +11,8 @@ urlpatterns= [
     url(r'^accounts/edit/',views.edit_profile, name='edit_profile'),
     url(r'^upload/$', views.profile_update, name='profile_update'),
     url(r'^upload/image/', views.post_image, name='post_image'),
+    url(r'^search/', views.search, name='search'),
+
 ]
 
 if settings.DEBUG:
